06577bf9edae678f67b839adcfb10bffef0fb0f6
James Sevedge <j.sevedge@f5.com>
2018-09-28 17:39:01 -0400

    init for post comments

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,11 +1,24 @@
 var loadedData = {};
 
 function getData() {
-    jQuery.getJSON("https://sevedgeignitefa.azurewebsites.net/api/GetComments", function (data) {
+    const url = "https://sevedgeignitefa.azurewebsites.net/api/GetComments"
+    jQuery.getJSON(url, function (data) {
         loadedData = data;
     }).then(function () {
         // update view
         updateView("#comments-table", loadedData);
+    });
+}
+
+function postData(url, data) {
+    $.ajax({
+        type: "POST",
+        url,
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json"
+    }).then(function () {
+        console.log(`Data POSTed`)
     });
 }
 
@@ -25,4 +38,13 @@ function updateView(el, obj) {
     var listEl = $(el);
     listEl.empty();
     listEl.append(rows.join("")); // note: defaults to comma...
+}
+
+function writeComment() {
+    const url = "https://sevedgeignitefa.azurewebsites.net/api/PostComments";
+    const data = document.getElementById("newComment").value;
+    commentData = {
+        'text': data
+    };
+    postData(url, commentData);
 }
